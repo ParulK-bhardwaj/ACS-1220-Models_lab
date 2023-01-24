9bda9f1ff019cd836cf25927ad6938f09fc37a20
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2023-01-23 19:37:49 -0500

    Added user Class.

--- books_app/models.py
+++ books_app/models.py
@@ -25,12 +25,14 @@ class Book(db.Model):
     # The genres, e.g. fiction, sci-fi, fantasy
     genres = db.relationship('Genre', secondary='book_genre', back_populates='books')
 
+    users = db.relationship('User', secondary='favorite_book', back_populates='books')
+
     def __str__(self):
         return f'<Book: {self.title}>'
 
     def __repr__(self):
-        return f'<Book: {self.title}>'
-
+        return f"""<{self.id}: {self.title}, {self.publish_date}>"""
+                
 class Author(db.Model):
     """Author model."""
     id = db.Column(db.Integer, primary_key=True)
@@ -59,3 +61,22 @@ book_genre_table = db.Table('book_genre',
     db.Column('book_id', db.Integer, db.ForeignKey('book.id')),
     db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'))
 )
+
+class User(db.Model):
+    """User model."""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    favorite_books = db.relationship('Book', secondary='favorite_book', back_populates='users')
+
+    def __str__(self):
+        return f'<User: {self.username}>'
+
+    def __repr__(self):
+        return f"""<{self.id}: {self.username}>"""
+
+favorite_book_table = db.Table('favorite_book',
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
